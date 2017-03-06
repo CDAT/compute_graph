814b3cf02e206debe132655dc2f1ed0bd6346d7c
Sam Fries <fries2@llnl.gov>
2017-03-06 11:46:53 -0800

    Bumped versions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
   name="compute_graph",
-  version="0.0.1",
+  version="1.1",
   description="A graph-based representation of operations performed on source data to generate data products.",
   url="http://github.com/UV-CDAT/compute_graph",
   packages=['compute_graph'],
